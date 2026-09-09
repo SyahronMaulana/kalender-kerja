2e0ec3abb5934a6e48b8eb1dade39b8e9a2fd1da
unknown <syahronmaulana123@gmail.com>
2026-09-09 07:21:58 +0700

    debug laravel error

--- api/index.php
+++ api/index.php
@@ -1,7 +1,20 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 echo "Before Laravel<br>";
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
 
-echo "<br>After Laravel";
+    echo "<br>After Laravel";
+} catch (\Throwable $e) {
+    echo "<h2>Laravel Error</h2>";
+    echo "<pre>";
+    echo $e->getMessage();
+    echo "\n\nFILE: " . $e->getFile();
+    echo "\nLINE: " . $e->getLine();
+    echo "\n\nTRACE:\n" . $e->getTraceAsString();
+    echo "</pre>";
+}
