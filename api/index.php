967e0e251424966ee4cce1df72fd12bd6da3266f
SyahronMaulana <syahronmaulana123@gmail.com>
2026-09-09 16:15:45 +0700

    test deploy

--- api/index.php
+++ api/index.php
@@ -1,20 +1,2 @@
-<?php
-
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-echo "Before Laravel<br>";
-
-try {
-    require __DIR__ . '/../public/index.php';
-
-    echo "<br>After Laravel";
-} catch (\Throwable $e) {
-    echo "<h2>Laravel Error</h2>";
-    echo "<pre>";
-    echo $e->getMessage();
-    echo "\n\nFILE: " . $e->getFile();
-    echo "\nLINE: " . $e->getLine();
-    echo "\n\nTRACE:\n" . $e->getTraceAsString();
-    echo "</pre>";
-}
+<?php 
+require __DIR__. '/../public/index.php';
